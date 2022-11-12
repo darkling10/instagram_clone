061c56aec35d2e09de7083e3e6df90733cb99f5f
Abbas Pathan <pathan.abbas10@gmail.com>
2022-11-12 14:15:35 +0530

    Project added

---
+++ lib/utils/config.dart
@@ -0,0 +1,8 @@
+// Initialize Firebase
+
+const apiKey = "AIzaSyCnv0iYucVSCGMQ-WLdb2uWobxZibQSzzY";
+const authDomain = "instagram-clone-fdde9.firebaseapp.com";
+const projectId = "instagram-clone-fdde9";
+const storageBucket = "instagram-clone-fdde9.appspot.com";
+const messagingSenderId = "508938762928";
+const appId = "1:508938762928:web:e75a7fc4950cc504415975";
